43c97fc995e28dbc0a031e39866666e5c8262823
Rana Nayek <134631149+RuneKingRana@users.noreply.github.com>
2024-03-08 01:04:15 +0530

    Add files via upload

---
+++ Design 3.py
@@ -0,0 +1,24 @@
+import turtle as t
+import colorsys as cs
+t.bgcolor('black') 
+t.width(8)
+t.speed(0)
+t.tracer (30)
+h= 0
+
+for i in range(600):
+    c = cs.hsv_to_rgb(h,1,1)
+    h += 0.005
+    t.color(c)
+    t.fillcolor('black')
+    t.begin_fill() 
+    t.circle(i/4, 60)
+    t.rt(20)
+    t.up()
+    t.goto(0,0)
+    t.down()
+    t.circle(i/4, 30)
+    t.rt(55)
+    t.fd(1/6) 
+    t.end_fill()
+t.done()
